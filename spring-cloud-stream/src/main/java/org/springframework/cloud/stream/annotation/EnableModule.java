cd3dffaf8e75af9eb803c89cc74857138065bf22
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2015-09-01 19:46:19 -0700

    Support RxJava module
     - Add a way to enable `RxJava` module
       - Add `EnableRxJavaModule` annotation that inherits `EnableModule` for `Processor` type.
          - It also registers the custom `MessageHandler` that sets up RxJava `Observable/Subscriber` to process the incoming message reactively.
          - Introduce `RxJavaProcessor` that is expected to be implemented by the module author with the input/output being `Observable`. This processor is autowired into the message handler registered above.
     - Add moving average sample

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/annotation/EnableModule.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/annotation/EnableModule.java
@@ -37,7 +37,7 @@ import org.springframework.integration.config.EnableIntegration;
  * @author Marius Bogoevici
  * @author David Turanski
  */
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
